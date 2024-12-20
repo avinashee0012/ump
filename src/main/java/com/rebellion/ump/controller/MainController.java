969796256f86e3849adf87d80b11e4f4b17bce93
Rebellion <avinashee0012@gmail.com>
2024-12-20 12:11:52 +0530

    Developed some frontend pages.

--- src/main/java/com/rebellion/ump/controller/MainController.java
+++ src/main/java/com/rebellion/ump/controller/MainController.java
@@ -2,15 +2,16 @@ package com.rebellion.ump.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("")
-public class Home {
+public class MainController {
 
     @GetMapping("")
-    public String getHome() {
-        return "Welcome to User Management Portal (UMP)";
+    public ModelAndView getHome() {
+        return new ModelAndView("redirect:/index.html");
     }
     
 }
